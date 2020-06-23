f97694b3c8546757e1f4242bf90c846d2eabb940
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-06-23 21:53:34 +0300

    Reading CSV File into Pandas Dataframe

--- ML_Model_Training/naive_bayes_script.py
+++ ML_Model_Training/naive_bayes_script.py
@@ -1,2 +1,8 @@
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
+import pandas as pd 
+
+
+# Importing Tweets from the CSV file
+df = pd.read_csv('data/twitter1.6m.csv', encoding='utf-8')
+
+# Take a look inside the dataset
+print(df.head())
