0c2a7763e27b5b40b6f69da60905ccd4d627093f
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-06-24 22:25:04 +0300

    Cleaning the Dataset
    Dropping any unnecessary rows and keeping the columns important in the Sentiment Analysis Task : "text", "target"

--- ML_Model_Training/naive_bayes_script.py
+++ ML_Model_Training/naive_bayes_script.py
@@ -1,8 +1,20 @@
 import pandas as pd 
 
 
-# Importing Tweets from the CSV file
+## Importing Tweets from the CSV file
 df = pd.read_csv('data/twitter1.6m.csv', encoding='utf-8')
+df.columns =['target','ids','date','flag','user','text']
 
-# Take a look inside the dataset
-print(df.head())
+## Take a look inside the dataset
+# print(df.head())
+# print(df.describe)
+# print(df.columns)
+# print(df.info())
+# print(df.isnull().sum())
+
+## Clean the Dataset - Reformatting it to drop columns i do not need for this model
+dataset = df[['text', 'target']]
+print(dataset.head())
+print(dataset.tail())
+print(dataset.shape)
+print(dataset.columns)
