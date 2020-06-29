5c65533c11d97b14cab0f20704ecbaf7de2c92c9
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-06-29 23:46:21 +0300

    Data Pre-Processing
    Working on Stop-words Removal

--- ML_Model_Training/naive_bayes_script.py
+++ ML_Model_Training/naive_bayes_script.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 ## Importing Tweets from the CSV file
 df = pd.read_csv('data/twitter1.6m.csv', encoding='utf-8')
@@ -25,7 +26,19 @@ y = dataset['target'] # These are the scores [0-4] :> int64
 # print(y.tail())
 
 ## Tokenization
-# X_tokens = [word_tokenize(word) for word in X] # Returns a List of Lists containing tokens
-# print(X_tokens[-1])
+X_tokens = [word_tokenize(word) for word in X] # Returns a List of Lists containing tokens
+print(len(X_tokens)) #1599999
+print(X_tokens[-1])
 
-## Stop-words Removal
+# ## Stop-words Removal
+# stop_words = set(stopwords.words('english'))
+# X_tokens_stopwords = []
+# for tweet_token_list in X_tokens:
+# 	new_list = []
+# 	for word in tweet_token_list:
+# 		if word not in stop_words:
+# 			new_list.append(word)
+	
+# 	X_tokens_stopwords.append(new_list)
+
+# print(len(X_tokens_stopwords))
