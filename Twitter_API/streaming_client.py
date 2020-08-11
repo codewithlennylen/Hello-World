f37a58e6f7ea2170bc8306b8b6a9861c73125dab
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-08-11 22:30:10 +0300

    Twitter API Stream

---
+++ Twitter_API/streaming_client.py
@@ -0,0 +1,36 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+
+secrets = json.load(open(
+    'C:/Users/lenovo/Documents/GitHub/Hello-World/Twitter_API/secrets.json', 'r'))
+
+
+#consumer key, consumer secret, access token, access secret.
+ckey = secrets['CONSUMER_KEY']
+csecret = secrets['CONSUMER_SECRET']
+atoken = secrets['ACCESS_TOKEN']
+asecret = secrets['ACCESS_SECRET']
+
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        # print(data)
+        all_data = json.loads(data)
+        # json.dump(all_data,open('sample_tweet.json','w'))
+        tweet = all_data['text']
+        print(tweet)
+        # print(all_data.keys())
+        return(True)
+
+    def on_error(self, status):
+        print (status)
+
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["100DaysofCode"])
